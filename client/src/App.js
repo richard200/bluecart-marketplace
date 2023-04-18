3f5d24e532071af0bba97ba6f470d85a6d4db8de
richard200 <richmn200@gmail.com>
2023-04-18 14:26:13 +0300

    working on jumia scraper

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,9 @@ function App() {
   return (
     <div className="App">
   <h1>My Phone Store</h1>
+  <p className='open'>Look for and compare products based on price, 
+    rating and find which store is best to visit for the product you are looking for</p>
+    <br></br>
       <PhoneList />
     </div>
   );
--- client/src/components/Phones.js
+++ client/src/components/Phones.js
@@ -1,15 +1,20 @@
 import React, { useState, useEffect } from 'react';
+import '../App.css'
 
 function PhoneList() {
   const [products, setProducts] = useState([]);
+  const [products1, setProducts1] = useState([]);
+  // const [products2, setProducts2] = useState([]);
 
   useEffect(() => {
     async function fetchPhones() {
       const response = await fetch('http://localhost:3000/scrape');
      
       const data = await response.json();
-      console.log(typeof data.jumia);
-      setProducts(data.jumia);
+      // console.log(typeof data.kilimall);
+      setProducts(data.jiji);
+      setProducts1(data.jumia);
+      // setProducts2(data.)
       // console.log(setProducts);
     }
     fetchPhones();
@@ -17,26 +22,48 @@ function PhoneList() {
 
   return (
     <div>
-      <h1>Phone List</h1>
-      <table>
-        <thead>
-          <tr>
-            <th>Name</th>
-            <th>Price</th>
-           
-          
-          </tr>
-        </thead>
-        <tbody>
+      {/* <h1>Phone List</h1> */}
+   
+      <h1>Products from Jambo Shop</h1>
         { Array.isArray(products) && products.map((product, index) => (
-        <tr key={index}>
-          <td>{product.title}</td>
-      
-          <td>{product.price}</td>
-        </tr>
-      ))}
-        </tbody>
-      </table>
+  <div className="card" key={index}>
+{/*      
+    <img className="card-img-top" src={product.image} alt={product.title} /> */}
+    <div className="card-body">
+    <img src={product.img} alt={product.title}/>
+    <h3>Sold by Jambo Shop</h3>
+      <h5 className="card-title">Title: {product.title}</h5>
+      <p className="card-text">Price: {product.price}</p>
+      {/* <p className="card-text">Image: {product.img}</p> */}
+     
+    </div>
+  </div>
+))}
+ <br></br> 
+ <br></br>
+ <br></br>
+ <br></br>
+ <br></br>
+
+ <h1>Products from Jumia</h1>
+{ Array.isArray(products1) && products1.map((product, index) => (
+   
+  <div className="card" key={index}>
+   
+    {/* <img className="card-img-top" src={product.image} alt={product.title} /> */}
+    <div className="card-body">
+    <img src={product.img} alt={product.title}/>
+      <h3>Sold by Jumia</h3>
+      <h5 className="card-title">Title: {product.title}</h5>
+      <p className="card-text">Price: {product.price}</p>
+      {/* <p className="card-text">Image: {product.img}</p> */}
+
+    
+    </div>
+    </div>
+ 
+))}
+
     </div>
   );
 }
