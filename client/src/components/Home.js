6605f74911c03623e0e9fdff1742ba7fb74db89f
MakenaG <makenaglory44@gmail.com>
2023-04-20 02:09:24 +0300

    changed home.js

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -22,12 +22,12 @@ function Home() {
           fontWeight: "bold",
           color: "#212121"
         }}>
-          Shopcrawl: The ultimate tool for online shopping
+          BlueCart: The ultimate tool for online shopping
         </Typography>
         <Typography variant="h5" component="h2" style={{ 
           color: "#757575"
         }}>
-          Shopcrawl helps you find the best deals and products from various e-commerce sites. You can compare prices, ratings, delivery costs, and more with our smart algorithms. 
+         BlueCart helps you find the best deals and products from various e-commerce sites. You can compare prices, ratings, delivery costs, and more with our smart algorithms. 
         </Typography>
         <Button variant="contained" color="primary" href="/search" style={{ 
           marginTop: "20px"
@@ -80,7 +80,7 @@ function Home() {
               color: "#757575",
               marginBottom: "20px"
             }}>
-              "Shopcrawl is amazing! It saves me so much time and money when I shop online. I love how it shows me the best deals and products from different sites in one place."
+              "Bluecart is amazing! It saves me so much time and money when I shop online. I love how it shows me the best deals and products from different sites in one place."
             </Typography>
             <Typography variant="body2" component="p" style={{ 
               fontWeight: "bold",
@@ -92,7 +92,7 @@ function Home() {
           <div className="Home-testimonial-card">
         <img src="customer-2.jpg" alt="Customer 2" width="100px"/>
         <Typography variant="body1" component="p" style={{color: "#757575"}}>
-          “I use Shopcrawl every time I need to buy something online. It’s so easy and convenient to compare prices, ratings, delivery costs, and more. It helps me make smarter buying decisions.”
+          “I use BlueCart every time I need to buy something online. It’s so easy and convenient to compare prices, ratings, delivery costs, and more. It helps me make smarter buying decisions.”
         </Typography>
         <Typography variant="body2" component="p" style={{fontWeight: "bold", color: "#212121"}}>
           - John Lee
@@ -101,7 +101,7 @@ function Home() {
       <div className="Home-testimonial-card">
         <img src="customer-3.jpg" alt="Customer 3" width="100px"/>
         <Typography variant="body1" component="p" style={{color: "#757575"}}>
-          “Shopcrawl is the best app for online shopping. It has everything I need to find the perfect products for me. It also has a great customer service team that is always ready to help.”
+          “BlueCart is the best app for online shopping. It has everything I need to find the perfect products for me. It also has a great customer service team that is always ready to help.”
         </Typography>
         <Typography variant="body2" component="p" style={{fontWeight: "bold", color: "#212121"}}>
           - Lisa Chen
@@ -116,7 +116,7 @@ function Home() {
       <a href="/privacy">Privacy Policy</a>
     </div>
     <Typography variant="body2" component="p" style={{color: "#757575"}}>
-      © 2023 Shopcrawl. All rights reserved.
+      © 2023 BlueCart. All rights reserved.
     </Typography>
   </div>
   </div>
--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { AppBar, Toolbar, Typography, IconButton } from '@material-ui/core';
 import MenuIcon from '@material-ui/icons/Menu';
+import SearchBar from './SearchBar';
 
 function Navbar() {
 const handleClick = () => {
@@ -8,13 +9,14 @@ console.log('Menu icon clicked');
 };
 
 return (
+  
 <AppBar position="static" color="primary">
 <Toolbar>
 <IconButton edge="start" color="inherit" aria-label="menu" onClick={handleClick}>
 <MenuIcon />
 </IconButton>
 <Typography variant="h6" style={{flexGrow: 1}}>
-Shopcrawl
+BlueCart
 </Typography>
 <div>
 <a href="/search" style={{color: 'white', textDecoration: 'none', marginRight: '20px'}}>
