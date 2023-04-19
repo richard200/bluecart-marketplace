0417893f07c4394974657bd3ed994bcc63d7be44
Sundus <sundusaden105@gmail.com>
2023-04-19 11:04:58 +0300

    done

---
+++ api/app/controllers/users_controller.rb
@@ -0,0 +1,54 @@
+class UsersController < ApplicationController
+  before_action :set_user, only: %i[ show update destroy ]
+  # skip_before_action :authorized, only: [:create]
+
+  # GET /users
+  def index
+    @users = User.all
+
+    render json: @users
+  end
+
+  # GET /users/1
+  def show
+    render json: @user
+  end
+  
+
+  # POST to create new user
+  def create
+    @user = User.create(user_params)
+
+    if @user.valid?
+      @token = encode_token(user_id: @user.id)
+      render json: { user: @user, jwt: @token }, status: :created
+    else
+      render json: @user.errors, status: :unprocessable_entity
+    end
+  end
+
+  # PATCH/PUT /users/1
+  def update
+    if @user.update(user_params)
+      render json: @user
+    else
+      render json: @user.errors, status: :unprocessable_entity
+    end
+  end
+
+  # DELETE /users/1
+  def destroy
+    @user.destroy
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_user
+      @user = User.find(params[:id])
+    end
+
+    # Only allow a list of trusted parameters through.
+    def user_params
+      params.permit(:username, :email, :password)
+    end
+end
--- api/config/routes.rb
+++ api/config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
+  # Route to create a new user
+  post '/signup', to: 'users#create'
+
+  post '/login', to: 'auth#create'
+
+  get '/users/all' , to: 'users#index'
+
   # Defines the root path route ("/")
   # root "articles#index"
 end
